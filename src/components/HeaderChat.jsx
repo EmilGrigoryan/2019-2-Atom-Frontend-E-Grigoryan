ad8f0b4a655a86531d9137b0eef9bac9c8bd2fa1
EmilGrigoryan <grigoryan.emil2017@yandex.ru>
2019-11-19 04:47:40 +0300

    fix header

--- src/components/HeaderChat.jsx
+++ src/components/HeaderChat.jsx
@@ -2,18 +2,12 @@ import React from 'react'
 
 import styles from '../styles/HeaderChat.module.css'
 
-export class HeaderChat extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className={styles.wrap}>
-        <div className={styles.burger}></div>
-        <div className={styles.data}>Chat</div>
-        <div className={styles.search}></div>
-      </div>
-    )
-  }
+export function HeaderChat(props) {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.burger}></div>
+      <div className={styles.data}>Chat</div>
+      <div className={styles.search}></div>
+    </div>
+  )
 }
